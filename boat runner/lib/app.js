9b8e0ad5704f9617f5b402cdb52bf54054d1c9d7
Helium5t <10533138@polimi.it>
2020-09-10 19:14:30 +0200

    linear texture filtering

--- boat runner/lib/app.js
+++ boat runner/lib/app.js
@@ -437,8 +437,8 @@ var textureLoaderCallback = function () {
 	gl.bindTexture(gl.TEXTURE_2D, textureId);
 	gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, this);
 	// set the filtering so we don't need mips
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
-	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.REPEAT);
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.REPEAT);
 }
